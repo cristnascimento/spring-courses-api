b95ee22e47d8f3cdd302da11e2fde759115f1ddd
Cristiano Nascimento <cristnascimento@gmail.com>
2020-10-17 16:14:03 -0300

    Compiling first project.

---
+++ coursesapp/src/test/java/com/github/cristnascimento/coursesapp/CoursesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cristnascimento.coursesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
